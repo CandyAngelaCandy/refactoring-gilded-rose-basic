50b4a3880a42a74969e65080500666b11211a000
Linxiao <linxiao.wei@rea-group.com>
2020-05-20 20:34:09 +0800

    [yanyan/linxiao] make item attributes private and rename sellIn

--- src/main/java/cn/xpbootcamp/gilded_rose/Item.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -2,21 +2,21 @@ package cn.xpbootcamp.gilded_rose;
 
 public class Item {
 
-    public String name;
+    private final String name;
 
-    public int sell_in;
+    private int sellIn;
 
-    public int quality;
+    private int quality;
 
     public Item(String name, int sell_in, int quality) {
         this.name = name;
-        this.sell_in = sell_in;
+        this.sellIn = sell_in;
         this.quality = quality;
     }
 
     @Override
     public String toString() {
-        return this.name + ", " + this.sell_in + ", " + this.quality;
+        return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 
     public void updateItemQuality() {
@@ -32,13 +32,13 @@ public class Item {
                 quality = quality + 1;
 
                 if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (sell_in < 11) {
+                    if (sellIn < 11) {
                         if (quality < 50) {
                             quality = quality + 1;
                         }
                     }
 
-                    if (sell_in < 6) {
+                    if (sellIn < 6) {
                         if (quality < 50) {
                             quality = quality + 1;
                         }
@@ -48,10 +48,10 @@ public class Item {
         }
 
         if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            sell_in = sell_in - 1;
+            sellIn = sellIn - 1;
         }
 
-        if (sell_in < 0) {
+        if (sellIn < 0) {
             if (!name.equals("Aged Brie")) {
                 if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (quality > 0) {
